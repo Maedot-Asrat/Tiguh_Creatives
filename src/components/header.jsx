aa6825d8d85aaf85323656bccd63d2bda7a580c2
Maedoot Asrat <asratmaedot@gmail.com>
2024-09-24 12:52:55 +0300

    frontend

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    
+    <section id="home" className="section">
+    <video autoPlay loop muted>
+      <source src="/img/tiguhmain.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </section>
+  );
+};
